eaee7fa6c9f5528db86a3f83ef22cdf60aaa16f1
Jason Benjoya <jasonbenjoya@gmail.com>
2020-11-14 12:28:35 -0600

    new model created for posts

--- models.py
+++ models.py
@@ -1,5 +1,6 @@
 """Models for Blogly."""
 from flask_sqlalchemy import SQLAlchemy
+import datetime
 
 DEFAULT_IMG_URL = 'https://www.freeiconspng.com/uploads/person-icon-user-person-man-icon-4.png'
 
@@ -27,3 +28,16 @@ class User(db.Model):
     def full_name(self):
         """render full firstname lastname" representation of user"""
         return f"{self.first_name} {self.last_name}"
+
+
+class Post(db.Model):
+    __tablename__ = "posts"
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    title = db.Column(db.Text, nullable=False, default="NEW POST")
+    content = db.Column(db.Text, nullable=False)
+    created_at = db.Column(db.DateTime, nullable=false,
+                           default=datetime.datetime.now)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+
+    user = db.relationship('User', backref='posts')
